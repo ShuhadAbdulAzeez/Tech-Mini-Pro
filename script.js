157f78352b7f71248d79d1293fed2bed9bd9909c
Shuhad Abdul Azeez <shuhad04@gmail.com>
2022-09-05 22:08:59 +0530

    added

--- script.js
+++ script.js
@@ -21,4 +21,28 @@ window.onscroll = ()=>{
 }
 
 
- 
+let slides = document.querySelectorAll('.slide-contai');
+let index = 0;
+
+
+
+// Background 
+
+//next function
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1)% slides.length;
+    slides[index].classList.add('active');
+}
+
+//prev function
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length)% slides.length;
+    slides[index].classList.add('active');
+}
+
+//autoplay
+
+setInterval(next, 5000); //slides changes every 7seconds
