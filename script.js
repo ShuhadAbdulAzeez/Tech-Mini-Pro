2ff0f7006f5a6dd4e09f572609f2b40123e8b316
Shuhad Abdul Azeez <shuhad04@gmail.com>
2022-09-16 16:30:34 +0530

    modified & added

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 
-
-// Navbar Responsive box
-
-const body = document.querySelector("body");
-const navbar = document.querySelector(".navbar");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
-  navbar.classList.add("show");
-  menuBtn.classList.add("hide");
-  body.classList.add("disabled");
-}
-cancelBtn.onclick = ()=>{
-  body.classList.remove("disabled");
-  navbar.classList.remove("show");
-  menuBtn.classList.remove("hide");
-}
-window.onscroll = ()=>{
-  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
-
+// Background  Slider//
 
 let slides = document.querySelectorAll('.slide-contai');
 let index = 0;
 
-
-
-// Background 
-
-//next function
+/**next function**/
 function next(){
     slides[index].classList.remove('active');
     index = (index + 1)% slides.length;
     slides[index].classList.add('active');
 }
 
-//prev function
+/**prev function**/
 
 function prev(){
     slides[index].classList.remove('active');
     index = (index - 1 + slides.length)% slides.length;
     slides[index].classList.add('active');
+} 
+
+/**autoplay**/
+
+setInterval(next, 7000); //slides changes every 7seconds
+
+/************************************************************/ 
+
+// feedback review//
+
+let userTexts = document.getElementsByClassName("usertext");
+let userPics = document.getElementsByClassName("user-pic")
+
+function showReview(){
+    for(userPic of userPics){
+        userPic.classList.remove("active-pic")
+    }
+
+    for(userText of userTexts){
+        userText.classList.remove("active-text")
+    }
+
+    let i = Array.from(userPics).indexOf(event.target)
+
+    userPics[i].classList.add("active-pic")
+    userTexts[i].classList.add("active-text")
 }
-
-//autoplay
-
-setInterval(next, 5000); //slides changes every 7seconds
